3856a4af7cd23f900ee2313a386de950b2d3eb96
limwelyane-dev <limwelyane@gmail.com>
2026-03-22 09:53:05 +0800

    Update ren.js

--- ren.js
+++ ren.js
@@ -11,5 +11,19 @@ document.getElementById(id).style.display="block";
 }
 
 function toggleMenu(){
+    const isMobile = window.innerWidth <= 900;
+
     document.querySelector("header").classList.toggle("active");
+
+    if(isMobile){
+        document.querySelector(".overlay").classList.toggle("active");
+        document.querySelector("main").classList.toggle("blur");
+    }
 }
+window.addEventListener("resize", () => {
+    if(window.innerWidth > 900){
+        document.querySelector("header").classList.remove("active");
+        document.querySelector(".overlay").classList.remove("active");
+        document.querySelector("main").classList.remove("blur");
+    }
+});
